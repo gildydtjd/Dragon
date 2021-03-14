887fb7f31a0ff118078cf85df50957a7fbcbd3e8
gildydtjd <gildydtjd@naver.com>
2021-03-14 21:55:46 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+const Divpage = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.color};
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divpage color="#bdc3c7"></Divpage>
+      <Divpage color="green"></Divpage>
+      <Divpage color="red"></Divpage>
     </div>
   );
 }
